3fa0be4c539978fae5af8a89505bf6ae5004ba68
Iván Alfredo López Barrera <ivan23.barrera@gmail.com>
2025-06-17 21:56:14 -0600

    Feat: The game logic is created

--- public/js/memory.js
+++ public/js/memory.js
@@ -1,17 +1,108 @@
 class Memory {
     #$templateGameCard;
-    #$gameCards
+    #$gameCards;
+    #$cards;
+    #completeCards;
+    #flippedCards;
     #currentMode;
     #modes;
     #emojis;
-    #isPlaying;
+    #game;
 
     /**
-     * Configures the game cards, setting rows and columns of the current mode
+     * Reposition the current cards in the game cards
      */
-    #configureCards(rows, cols) {
+    #repositionCards(rows, cols) {
+        // Set the new columns in the game cards grid
+        this.#$gameCards.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
+
+        // Gets all the current cards
+        const currentCards = this.#$gameCards.querySelectorAll('.game__card');
+
+        // Reconfigure all the cards in the game according to the rows and cols
+        for (let i = 0; i < rows; i++) {
+            for (let j = 0; j < cols; j++) {
+                // Get the current card
+                const currentCard = currentCards[j + i * cols];
+
+                // Put the new position in the current card
+                currentCard.style.gridRow = `${i + 1}/${i + 2}`;
+                currentCard.style.gridColumn = `${j + 1}/${j + 2}`;
+            }
+        }
+    }
+
+    /**
+     * Gets the emojis that will be used in the game
+     */
+    #getGameEmojis() {
+        const emojis = [];
+
+        // Get the amount of emojis according to the current mode
+        const amountEmojis = this.#modes[this.#currentMode].rows * 2;
+
+        // Get the total emojis
+        const totalEmojis = this.#emojis.length;
+
+        // Set the current emojis
+        for (let i = 0; i < amountEmojis; i++) {
+            const currentEmoji = this.#emojis[
+                Math.floor(Math.random() * totalEmojis)
+            ];
+
+            // Check that the current emoji has not been previously selected
+            if (emojis.includes(currentEmoji)) {
+                i--;
+                continue;
+            }
+
+            // Adds the current emoji to the emojis list
+            emojis.push(currentEmoji);
+        }
+
+        return emojis;
+    }
+
+    /**
+     * Sets the emojis distribution of the current game
+     */
+    #setCurrentGame() {
+        // Clear the game
+        this.#game = [];
+
+        // Get the selected emojis
+        const selectedEmojis = this.#getGameEmojis();
+
+        // Put randomly the selected emojis in the game
+        while (selectedEmojis.length > 0) {
+            // Select randomly one emoji
+            const emojiIdx = Math.floor(Math.random() * selectedEmojis.length);
+            const currentEmoji = selectedEmojis[emojiIdx];
+
+            // Check if the game already had the current emoji included
+            if (this.#game.includes(currentEmoji)) {
+                // Remove the current emoji from the selected emojis
+                selectedEmojis.splice(emojiIdx, 1);
+            }
+
+            // Add the current emoji to the game
+            this.#game.push(currentEmoji);
+        }
+    }
+
+    /**
+     * Create the game cards, setting rows and columns of the current mode
+     */
+    #createCards(rows, cols) {
         // Set the columns in the game cards grid
         this.#$gameCards.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
+
+        // Clear the flipped and complete cards
+        this.#flippedCards.length = 0;
+        this.#completeCards.length = 0;
+
+        // Delete the old cards
+        this.#$gameCards.innerText = '';
 
         // Generate and put a card in the game according to the rows and cols
         for (let i = 0; i < rows; i++) {
@@ -25,16 +116,26 @@ class Memory {
                 gameCardCopy.style.gridRow = `${i + 1}/${i + 2}`;
                 gameCardCopy.style.gridColumn = `${j + 1}/${j + 2}`;
 
+                // Puts the current card index inside the element
+                gameCardCopy.setAttribute('data-idx', j + i * cols);
+
                 // Insert the copy in the game cards
                 this.#$gameCards.appendChild(gameCardCopy);
             }
         }
+
+        // Set the current game
+        this.#setCurrentGame();
+
+        // Get all the game cards
+        this.#$cards = this.#$gameCards.querySelectorAll('.game__card');
     }
 
     /**
-     * Sets the game according to the current mode
+     * Sets the game according to the current mode. This method executes the
+     * method passed as a parameter to position the cards
      */
-    #setGame() {
+    #setGame(positionMethod) {
         // Get the rows and columns of the current mode
         const rows = this.#modes[this.#currentMode].rows;
         const cols = this.#modes[this.#currentMode].columns;
@@ -45,10 +146,90 @@ class Memory {
 
         // The columns will be configured depending on the screen size
         if (height > width) {  // Vertical
-            this.#configureCards(rows, cols);
+            positionMethod(rows, cols);
         } else {  // Horizontal (invert rows by columns)
-            this.#configureCards(cols, rows);
+            positionMethod(cols, rows);
         }
+    }
+
+    /**
+     * Sets the game events to change the current mode and play the game
+     */
+    #gameEvents() {
+        // Event when the user changes the current mode
+        const gameOptions = document.querySelector('#game__options');
+        gameOptions.addEventListener('click', (event) => {
+            // Get the clicked element
+            const target = event.target;
+
+            // Verify if the clicked element is not an option
+            if (!target.classList.contains('game__option')) {
+                return;
+            }
+
+            // Change the current mode
+            this.#currentMode = target.innerText;
+            this.#setGame(this.#createCards.bind(this));
+        });
+
+        // Event when the user makes a play
+        this.#$gameCards.addEventListener('click', (event) => {
+            // Get the clicked element
+            const target = event.target;
+
+            // Verify if the clicked element is not a card
+            if (!target.classList.contains('game__card')) return;
+
+            // Get the card index
+            const idx = target.getAttribute('data-idx');
+
+            // Check that the user doesn't click on the same card or one complete
+            // card
+            if (this.#flippedCards.includes(idx) ||
+                this.#completeCards.includes(idx)) {
+                return;
+            }
+
+            // Get the current emoji
+            const currentEmoji = this.#game[idx];
+
+            // Shows the current emoji
+            target.innerText = currentEmoji;
+
+            // Flip and hide the cards
+            switch (this.#flippedCards.length) {
+                case 0:
+                    // Add the current index to the flipped cards
+                    this.#flippedCards.push(idx);
+                    break;
+                case 1:
+                    // Check if the new emoji is the same as the old one
+                    const lastIdx = this.#flippedCards.at(-1);
+                    const lastEmoji = this.#game[lastIdx];
+
+                    if (currentEmoji == lastEmoji) {
+                        // Add the indexes to the complete cards
+                        this.#completeCards.push(idx, lastIdx);
+
+                        // Clear the flipped cards
+                        this.#flippedCards.length = 0;
+                    } else {
+                        // Add the current index to the flipped cards
+                        this.#flippedCards.push(idx);
+                    }
+                    break;
+                default:
+                    // Hides the flipped cards
+                    this.#flippedCards.forEach((cardIdx) => {
+                        const card = this.#$cards[cardIdx];
+                        card.innerText = '';
+                    });
+
+                    // Clear the flipped cards list
+                    this.#flippedCards.length = 0;
+                    this.#flippedCards.push(idx);
+            }
+        })
     }
 
     /**
@@ -56,12 +237,14 @@ class Memory {
      */
     #setDeviceEvents() {
         window.addEventListener('resize', () => {
-            this.#setGame();
+            this.#setGame(this.#repositionCards.bind(this))
         })
     }
 
     constructor() {
         // Configure the attributes
+        this.#completeCards = [];
+        this.#flippedCards = [];
         this.#currentMode = '4×4';
         this.#modes = {
             '4×4': { rows: 4, columns: 4 },  // 16 cards
@@ -79,17 +262,19 @@ class Memory {
             '🐶', '🐱', '🦊', '🐻', '🦆', '🐒', '🦉', '🐜', '🦞', '🦈',
             '🐊', '🐂', '🐖', '🐓', '🐈', '🐉', '🌎', '🪐', '⛄️', '🌊',
         ];
-        this.#isPlaying = false;
 
         // Get the DOM elements
         this.#$templateGameCard = document.querySelector('#template-game-card');
         this.#$gameCards = document.querySelector('#game-cards');
 
         // Set the current game
-        this.#setGame();
+        this.#setGame(this.#createCards.bind(this));
 
         // Set the device events
         this.#setDeviceEvents();
+
+        // Set game events
+        this.#gameEvents();
     }
 }
 
